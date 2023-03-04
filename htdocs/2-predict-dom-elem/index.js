278495d4e0f7a80a99e9c0e63f77d12aa836c173
Jordan Pratt <jpratt@mtroyal.ca>
2023-03-04 20:34:26 +0000

    Sanity sync

--- htdocs/2-predict-dom-elem/index.js
+++ htdocs/2-predict-dom-elem/index.js
@@ -15,8 +15,26 @@ let players = [
 
 function playerDiv(player) {
   let div = document.createElement("div");
+  div.id = "container";
+
+  let h1 = document.createElement("h1");
+  h1.append("Player Spotlight");
+
   let nickSpan = document.createElement("span");
   nickSpan.textContent = player.nick;
 
   let scoreSpan = document.createElement("span");
+  scoreSpan.textContent = player.highScore;
+
+  if (Number.parseInt(player.highScore) > 200) {
+    scoreSpan.classList.add("elite");
+  }
+
+  div.append(nickSpan, scoreSpan);
+
+  return div;
 }
+
+let pThings = players.map((p) => playerDiv(p));
+
+document.querySelector("body").append(pThings[1]);

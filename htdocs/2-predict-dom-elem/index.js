25fbea4200d1705a9b7af1cdf32ddb74fc108c99
Jordan Pratt <jpratt@mtroyal.ca>
2023-03-04 19:29:02 +0000

    Sync

---
+++ htdocs/2-predict-dom-elem/index.js
@@ -0,0 +1,22 @@
+let players = [
+  {
+    nick: "sm00th",
+    highScore: 212,
+  },
+  {
+    nick: "r3d h3d",
+    highScore: 301,
+  },
+  {
+    nick: "@@sMILz@@",
+    highScore: 192,
+  },
+];
+
+function playerDiv(player) {
+  let div = document.createElement("div");
+    let nickSpan = document.createElement("span");
+    nickSpan.textContent = player.nick;
+    
+  let scoreSpan = document.createElement("span");
+}
